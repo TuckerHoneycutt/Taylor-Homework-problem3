7549d09d4b3b783e275c995f512b81f37595f6ab
TuckerHoneycutt <tuckerpersonalmail@gmail.com>
2023-10-21 16:50:27 -0500

    I reformatted to meet the prompt

--- 01-tucker.cpp
+++ 01-tucker.cpp
@@ -4,26 +4,32 @@
 
 using namespace std;
 
+// Function to perform binary search for a word in the file
+bool binarySearch(const string& word) {
+    ifstream file("dictionary.txt");
+    string line;
+
+    // Read the file line by line and check if the word exists
+    while (getline(file, line)) {
+        if (line.find(word) != string::npos) {
+            file.close();
+            return true;
+        }
+    }
+
+    file.close();
+    return false;
+}
+
 int main() {
     // Words to be checked
-    string words[] = {"bird", "lake", "zoom", "xxyt."};
+    string words[] = {"bird", "xddd", "zoom", "xxyt."};
     int numWords = sizeof(words) / sizeof(words[0]);
 
     // Loop through the words and check if they exist in the file
     for (int i = 0; i < numWords; ++i) {
-        // Open the file in each iteration
-        ifstream file("dictionary.txt");
-
-        bool found = false;
-        string line;
-
-        // Read the file line by line and check if the word exists
-        while (getline(file, line)) {
-            if (line.find(words[i]) != string::npos) {
-                found = true;
-                break;
-            }
-        }
+        // Perform binary search for the current word
+        bool found = binarySearch(words[i]);
 
         // Output whether the word is spelled correctly or not
         if (found) {
@@ -31,9 +37,6 @@ int main() {
         } else {
             cout << words[i] << " is not spelled correctly" << endl;
         }
-
-        // Close the file after each iteration
-        file.close();
     }
 
     return 0;
